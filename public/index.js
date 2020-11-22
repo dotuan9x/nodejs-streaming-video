b47312b4ccd9cecd5c3bd8f53cd1f7923507ede2
thinhdnp <thinhdnp@antsprogrammatic.com>
2020-11-22 10:13:46 +0700

    Update static

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT_NODE || process.env.PORT || 8000;
@@ -11,6 +12,7 @@ const port = process.env.PORT_NODE || process.env.PORT || 8000;
 app.use(cors());
 
 app.use('/api', require('@route'));
+app.use('/static', express.static(path.join(__dirname, '/../asset')));
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
